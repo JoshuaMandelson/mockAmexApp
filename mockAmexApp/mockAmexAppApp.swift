ac3f053d2403b7e97d9f854a06e701b4f3493747
Joshua Mandelson <joshmandelson5@gmail.com>
2025-10-30 14:16:37 -0500

    Initial Commit

---
+++ mockAmexApp/mockAmexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  mockAmexAppApp.swift
+//  mockAmexApp
+//
+//  Created by Joshua Mandelson on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct mockAmexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
